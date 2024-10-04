153629a216e17abe479a15406417738dda37f430
chasolow <anssach@gmail.com>
2024-10-05 04:52:27 +0700

    Обновление кода

--- TipTokCalc.py
+++ TipTokCalc.py
@@ -66,13 +66,15 @@ st.image(image)
 st.markdown("<h1>Расчет стоимости услуг</h1>", unsafe_allow_html=True)
 
 # Используем HTML для увеличенного текста
-st.markdown('<div class="large-text">Есть ли на объекте существующая мощность согласно техническим условиям?</div><br>', unsafe_allow_html=True)
+st.markdown('<div class="large-text">Есть ли на объекте существующая мощность согласно техническим условиям?</div>', unsafe_allow_html=True)
 power_question = st.radio("", ['⚡️ Да', '❌ Нет'], index=0)
+
 
 # Поля для ввода мощности с использованием number_input
 if power_question == '⚡️ Да':
     P = st.number_input("Введите суммарную мощность объекта (P, кВт):", min_value=0, max_value=500000, step=1, value=0)
-    Pdop = st.number_input("Введите дополнительную мощность (Pдоп, кВт):", min_value=0, max_value=500000, step=1, value=0)
+    st.markdown("Введите дополнительную мощность (P<sub>доп</sub>, кВт):", unsafe_allow_html=True)
+    Pdop = st.number_input("", min_value=0, max_value=500000, step=1, value=0)
 else:
     P = st.number_input("Введите суммарную мощность объекта (P, кВт):", min_value=0, max_value=500000, step=1, value=0)
     Pdop = None
