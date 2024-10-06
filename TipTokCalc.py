e7749fe436f4247ca1437ab5fc478298d7761ccd
chasolow <anssach@gmail.com>
2024-10-07 06:44:03 +0700

    Обновление кода

--- TipTokCalc.py
+++ TipTokCalc.py
@@ -1,32 +1,86 @@
 import streamlit as st
+import pandas as pd
 import requests
 from io import BytesIO
 from PIL import Image
-import pandas as pd
 import os
 
 # Подключаем стили
 st.markdown("""
     <style>
-        /* ваши стили */
+        @font-face {
+            font-family: 'Cascadia Mono';
+            src: url('https://cdn.jsdelivr.net/gh/microsoft/cascadia-code@v2009.22/CascadiaMono.woff2') format('woff2');
+        }
+        body {
+            font-family: 'Cascadia Mono', monospace;
+            color: #F4B03F;
+            background-color: #1e1e1e;  /* Цвет фона, если нужно */
+        }
+        h1, h3 {
+            color: #F4B03F !important;
+            text-align: center;  /* Выравнивание по центру */
+        }
+        .stButton button {
+            background-color: #F4B03F !important;
+            color: #535353 !important;
+            font-weight: bold !important;
+            font-size: 16px !important;
+            border: none !important;  /* Убираем рамку */
+        }
+        .stButton button:hover {
+            background-color: #F4B03F !important;  /* Цвет кнопки при наведении */
+        }
+        .stTextInput input {
+            border: 2px solid #F4B03F !important;  /* Цвет рамки текстовых полей */
+            color: #F4B03F !important;  /* Цвет текста */
+        }
+        .stTextInput input:focus {
+            border-color: #F4B03F !important;  /* Цвет рамки при фокусе */
+            box-shadow: 0 0 5px #F4B03F !important;  /* Эффект подсветки */
+        }
+        .large-text {
+            font-size: 18px; /* Увеличьте размер шрифта здесь */
+        }
+        img {
+            width: 100%;
+        }
+        @media screen and (max-width: 550px) {
+            img {
+                width: 100%;
+                height: auto;
+            }
+        }
     </style>
 """, unsafe_allow_html=True)
 
-# Загружаем изображение
+# URL изображения
 image_url = "https://i.postimg.cc/vZmHCG8k/big.png"
+
+# Загрузка изображения
 response = requests.get(image_url)
 image = Image.open(BytesIO(response.content))
+
+# Отображаем изображение
 st.image(image)
 
 # Заголовок
 st.markdown("<h1>Расчет стоимости услуг</h1>", unsafe_allow_html=True)
 
-# Ввод данных
+# Используем HTML для увеличенного текста
 power_question = st.radio("Есть ли на объекте существующая мощность согласно техническим условиям?", ['⚡️ Да', '❌ Нет'], index=0)
-P = st.number_input("Введите суммарную мощность объекта (P, кВт):", min_value=0, max_value=500000, step=1, value=None)
-Pdop = st.number_input("Введите дополнительную мощность (Pдоп, кВт):", min_value=0, max_value=500000, step=1, value=None) if power_question == '⚡️ Да' else None
 
-# Выбор класса напряжения
+# Поля для ввода мощности с использованием number_input
+if power_question == '⚡️ Да':
+    P = st.number_input("Введите суммарную мощность объекта (P, кВт):", min_value=0, max_value=500000, step=1, value=None)
+    Pdop = st.number_input("Введите дополнительную мощность (Pдоп, кВт):", min_value=0, max_value=500000, step=1, value=None)
+else:
+    P = st.number_input("Введите суммарную мощность объекта (P, кВт):", min_value=0, max_value=500000, step=1, value=None)
+    Pdop = None
+
+st.markdown("", unsafe_allow_html=True) # Разрыв
+
+# Выбор класса напряжения с помощью radio
 voltage_classes = {
     "До 1000 В": 1,
     "3 - 35 кВ": 1.1,
@@ -34,16 +88,36 @@ voltage_classes = {
     "220 кВ": 1.3
 }
 Ku = st.radio("Класс напряжения в точке присоединения:", list(voltage_classes.keys()))
+
+# Получаем значение Ku_value на основе выбранного класса напряжения
 Ku_value = voltage_classes[Ku]
 
-# Вопрос о компенсации реактивной мощности
+st.markdown("", unsafe_allow_html=True) # Разрыв
+
+# Вопрос о компенсации реактивной мощности 
 Ktg = st.radio("Есть ли в технических условиях пункт по компенсации реактивной мощности?", ['✅ Есть', '❌ Нет'])
+
+st.markdown("", unsafe_allow_html=True) # Разрыв
+
+# Вопрос о схемах питания
 schemes = st.radio("Есть ли схемы питания электроприемников на объекте?", ['✅ Есть', '❌ Нет'])
 
-X = st.number_input("Количество участков ЛЭП от центра питания до каждого электроприемника:", min_value=0, max_value=5000, step=1, value=None) if schemes == '✅ Есть' else 0
-Y = st.number_input("Количество электроприемников:", min_value=0, max_value=5000, step=1, value=None)
+if schemes == '✅ Есть':
+    X = st.number_input("Количество участков ЛЭП от центра питания до каждого электроприемника:", min_value=0, max_value=5000, step=1, value=None)
+    Y = st.number_input("Количество электроприемников:", min_value=0, max_value=5000, step=1, value=None)
+else:
+    Y = st.number_input("Количество электроприемников:", min_value=0, max_value=5000, step=1, value=None)
+    X = Y * 1.05
 
+st.markdown("", unsafe_allow_html=True) # Разрыв
+
+# Вопрос о сопровождении согласования 
 Kc = st.radio("Требуется ли сопровождение согласования РД в электрических сетях?", ['📝 Требуется', '❌ Не требуется'])
+
+st.markdown("", unsafe_allow_html=True) # Разрыв
+
+# Путь к файлу статистики
+statistics_file_path = r"C:\Users\wanss\OneDrive\Рабочий стол\TipTok\Calc_stat.xlsx"
 
 # Кнопка расчета
 if st.button('РАСЧЁТ'):
@@ -51,24 +125,38 @@ if st.button('РАСЧЁТ'):
         if P <= 0:
             st.error("Параметры должны быть больше нуля")
         else:
+            # Рассчитываем коэффициенты
             Kp = (0.8533 * Pdop ** 0.0599 + (0.8533 * P ** 0.0599 - 0.8533 * Pdop ** 0.0599) * Pdop / P) if Pdop else 0.8533 * P ** 0.0599
             Ktg_value = 1.1 if Ktg == '✅ Есть' else 1
             Kc_value = 1.05 if Kc == '📝 Требуется' else 1
 
-            Gx = 1892.9 * X ** -0.544
-            Gy = 379.89 * Y ** -0.271
-            Gz = 966.81 * 2 * X ** -0.424
+            if schemes == '✅ Есть':
+                Gx = 1892.9 * X ** -0.544
+                Gy = 379.89 * Y ** -0.271
+                Gz = 0  # Gz не рассчитываем
+            else:
+                Gx = 1892.9 * X ** -0.544
+                Gy = 379.89 * Y ** -0.271
+                Gz = 966.81 * 2 * X ** -0.424
 
+            # Общая стоимость
             cost = round(((X * Gx + Y * Gy) * Kp * Ku_value * Ktg_value * Kc_value + X * Gz) / 100) * 100
-            st.markdown(f"<h1 style='color: #F4B03F;'>Общая стоимость: {cost} рублей</h1>", unsafe_allow_html=True)
+            st.markdown(
+                f"""
+                <div style='background-color: rgba(46, 139, 87, 0.15); padding: 20px; border-radius: 10px; text-align: center;'>
+                    <h1 style='color: #F4B03F;'>Общая стоимость: {cost} рублей</h1>
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
 
-            # Сбор статистики
-            statistics_file_path = r"C:\Users\wanss\OneDrive\Рабочий стол\TipTok\Calc_stat.xlsx"
+            # Запись статистики в Excel
             if os.path.isfile(statistics_file_path):
                 df = pd.read_excel(statistics_file_path)
             else:
                 df = pd.DataFrame(columns=["№", "Тип услуги", "P", "Pдоп", "U", "КРМ", "Схемы", "Участки", "ЭП", "Согласование", "Стоимость"])
 
+            # Создание новой строки
             new_row = {
                 "№": len(df) + 1,
                 "Тип услуги": "КЭЭ",
@@ -83,17 +171,15 @@ if st.button('РАСЧЁТ'):
                 "Стоимость": cost
             }
 
-            st.write("Данные для записи: ", new_row)
-
+            # Добавление новой строки в DataFrame
             df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
 
             # Запись данных в Excel
-            try:
-                with pd.ExcelWriter(statistics_file_path, engine='openpyxl', mode='w') as writer:
-                    df.to_excel(writer, index=False)
-                st.success("Данные успешно записаны в файл.")
-            except Exception as e:
-                st.error(f"Ошибка записи в файл: {e}")
+            with pd.ExcelWriter(statistics_file_path, engine='openpyxl', mode='w') as writer:
+                df.to_excel(writer, index=False)
 
+            st.success("Данные успешно записаны в файл.")
+    except ZeroDivisionError:
+        st.error("Ошибка: деление на ноль невозможно.")
     except Exception as e:
         st.error(f"Ошибка: {e}")
